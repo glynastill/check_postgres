glynastill/check_postgres

commit 099a599e68459c16a7447b95bb10bb55384217c5

Better check for insufficient privileges and configuration options that haven't been set and are needed.

check_postgres.pl
@@ -7703,13 +7703,13 @@ sub check_txn_idle {
         my $st = $r->{state} || '';
 
         ## Return unknown if we cannot see because we are a non-superuser
-        if ($cq =~ /insufficient/o) {
+        if ($cq =~ /^insufficient/o) {
             add_unknown msg('psa-nosuper');
             return;
         }
 
         ## Return unknown if stats_command_string / track_activities is off
-        if ($cq =~ /disabled/o or $cq =~ /<command string not enabled>/) {
+        if ($cq =~ /^disabled/o or $cq =~ /^<command string not enabled>/) {
             add_unknown msg('psa-disabled');
             return;
         }

commit c35ee104b6495f16678820edafaa33e10201a94e

Fix check for insufficient privileges broken by commit 099a599e68459c16a7447b95bb10bb55384217c5

check_postgres.pl
@@ -8098,7 +8098,7 @@ sub check_txn_idle {
         my $st = $r->{state} || '';
 
         ## Return unknown if we cannot see because we are a non-superuser
-        if ($cq =~ /^insufficient/o) {
+        if ($cq =~ /^insufficient/o or $cq =~ /^<insufficient privilege>/) {
             add_unknown msg('psa-nosuper');
             return;
         }